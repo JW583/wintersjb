fe3c7582c5b74515d6aeffd232e3bbc0bb3f58fc
Matt Wilson <wilsonmatt@susqu.edu>
2022-11-07 11:38:59 -0500

    Wk9 graded

--- code/week-9/Winters_Week9_Ecosystems_Assignment.R
+++ code/week-9/Winters_Week9_Ecosystems_Assignment.R
@@ -32,6 +32,7 @@
   head(veg.means)
   
   #adjusting the data to fit into the RDA function, reducing to 6 rows each and removing the NA columns  
+  #Why did you reduce these to 6 rows each? That removed a lot of useful data.
   abiotic.means1 <- abiotic.means[,-16]
   abiotic.means1 <- abiotic.means1[14:19,]
   abiotic.means2 <- abiotic.means1[,-1:-6]
@@ -55,6 +56,7 @@
   ord
   anova(ord)  
   plot(ord)
+  #Removing all of those rows of data explains why these results look strange.
   
   #running analysis test to see which factors might have the most impact on the community
   ord.int <- rda(veg.means2 ~1, abiotic.means2)
@@ -67,7 +69,9 @@
   anova(ord2)
   plot(ord2)
   
-  #For this RDA, I wanted to see if the overall pH, along with the amount of nitrogen and phosphorous in the soil, had any effect on the plant population, which the analysis showed that the amount of pH and nitrogen have little to no effect on the success of the plant community, while phosphourous seems to be heavily correlated to the communities success. This most likely means that the amount of phosphorous within the soul dictates how much a plant species is able to populate and thrive within the ecosystem. 
+  #For this RDA, I wanted to see if the overall pH, along with the amount of nitrogen and phosphorous in the soil, had any effect on the plant population, which the analysis showed that the amount of pH and nitrogen have little to no effect on the success of the plant community, while phosphourous seems to be heavily correlated to the communities success. 
+  #This most likely means that the amount of phosphorous within the soul dictates how much a plant species is able to populate and thrive within the ecosystem. 
+  #But P was not a significant predictor of the community to say that.
   
   # (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
   # Explain the ecological importance of the significant predictors, or lack of significant predictors.
@@ -91,8 +95,11 @@
   plot(ord3)
 
   #Based on these few test, these seemed to be the most likely factors based on the analysis to have a strong correlation the plant communities success, which based on my models, seems to be that model 2 would be the best fit to show the driving abiotic factors that formulate the plant communities within the plots. Based on the RDA both the amount of magnesium and total amount of phosphorous within the soil composition determines what kind and how abundant a specific plant community is within the plots, with there being more plant diversity overall in places with good amounts of magnesium and phosphorous within the soil, with diversity drastically decreasing with less of those resources present.
+  #None of these are linear models as stated in the directions. They are all rda() instead of lm()
 
   # (Q3 - 6 pts) Provide a 3-4 sentence synthesis of how these results relate to one another and the value of considering both together for interpreting biotic-abiotic interactions.
   
-  #Based on my results, both magnesium and phosphorous are highly important abiotic factors when determining the various plant species that can thrive in these communities. Both magnesium and phosphorous concentrations affect the soil chemestry within these locations, which these nutrients the plants would be able to take in and use to grow and multiply, in this case plant diversity seemed to be most abundant in places with higher concentration of these two elements, versus plots where these elements were lacking having an overall lower diversity and abundance. Tying this into a bigger picture, these plants most likely correlate to more oxygen and nitrogen within the community, allowing the invertebrates and nematodes to benefit from the fix and be more diverse in the location with more fauna diversity, creating a stronger and more stable community than to places that were lacking in these resources that hindered these plants species overall success.
+  #Based on my results, both magnesium and phosphorous are highly important abiotic factors when determining the various plant species that can thrive in these communities. 
+  #Both magnesium and phosphorous concentrations affect the soil chemestry within these locations, which these nutrients the plants would be able to take in and use to grow and multiply, in this case plant diversity seemed to be most abundant in places with higher concentration of these two elements, versus plots where these elements were lacking having an overall lower diversity and abundance. 
+  #Tying this into a bigger picture, these plants most likely correlate to more oxygen and nitrogen within the community, allowing the invertebrates and nematodes to benefit from the fix and be more diverse in the location with more fauna diversity, creating a stronger and more stable community than to places that were lacking in these resources that hindered these plants species overall success.
 
