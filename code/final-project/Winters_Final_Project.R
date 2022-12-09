7249925292c04cd0d18f20a9af53577cb37670d6
JW583 <112492367+JW583@users.noreply.github.com>
2022-12-09 15:09:00 -0500

    Update Winters_Final_Project.R
    update to the code (still not finished)

--- code/final-project/Winters_Final_Project.R
+++ code/final-project/Winters_Final_Project.R
@@ -16,30 +16,12 @@ water.quality.sf <- st_as_sf(water.quality, coords = c("lat","long"))
 
 #Merge them together by location
 matched = st_join(walleye.local.sf, water.quality.sf, join = nngeo::st_nn, maxdist = 5000, k = 1, progress = FALSE)
+subset(matched, species == "walleye")
 
-head(matched)
+walleye.water.qaulity <- matched[-1:-29958,] # NA column
+head(walleye.water.qaulity)
+View(walleye.water.qaulity)
 
-
-
-
-#I would scrap what you have below here.
-walleye.local.names <- paste(walleye.local$lattitude, walleye.local$longitude)
-walleye.local$names <- walleye.local.names
-walleye.local.means <- aggregate(x = walleye.local, by = list(walleye.local$names), FUN = "mean")
-head(walleye.local.means)
-
-water.quality.names <- paste(water.quality$lat, water.quality$long)
-water.quality$names <- water.quality.names
-water.quality.means <- aggregate(x = water.quality, by = list(water.quality$names), FUN = "mean")
-head(water.quality.means)
-
-walleye.local.means1 <- walleye.local.means[,-8:-9]
-walleye.local.means2 <- walleye.local.means1[,-3:-5]
-#walleye.local.means3 <- walleye.local.means2[2:29959,]
-head(walleye.local.means2)
-
-water.quality.means1 <- water.quality.means[,-7]
-water.quality.means2 <- water.quality.means1[,-20]
-water.quality.means3 <- water.quality.means2[,-12:-13]
-head(water.quality.means3)
-View(water.quality.means3)
+colnames(walleye.water.qaulity)
+mod1 <- lm(species ~ temp.mean + cond.mean + ph.mean + turb.mean + chloro.mean + do.percent.mean + do.ppm.mean, walleye.water.qaulity)
+summary(mod1)
