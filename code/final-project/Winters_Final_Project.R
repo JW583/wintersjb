1f627d54011b615aa4cd641f14eb188ed51ff3de
JW583 <112492367+JW583@users.noreply.github.com>
2022-12-09 13:24:42 -0500

    Update Winters_Final_Project.R
    fixed

--- code/final-project/Winters_Final_Project.R
+++ code/final-project/Winters_Final_Project.R
@@ -1,22 +1,29 @@
 setwd("C:/GitHub/wintersjb/code/final-project")
-install.packages(c("sourcetools", "shiny"))
-install.packages("poppr")
-library(poppr)
 library(vegan)
-library(readxl)
 
-genes.tibble <- read_excel("White_et_al_Brook_Trout_Introgression.xlsx", sheet = "Microsatellite Genotypes")
-genes <- as.data.frame(genes.tibble)
-head(genes)
-trout.avg <- read.csv("Fig3_data_forMark.csv")
-head(trout.avg)
+walleye.local <- read.csv("Kraus-et-al-MEE-telemetry-detection-data-for-Erie-Winnipeg.csv")
+head(walleye.local)
 
-genes.names <- paste(genes$ID)
-genes$names <- genes.names
-genes.means <- aggregate(x = genes, by = list(genes$names), FUN = "mean")
-head(genes.means)
+water.quality <- read.csv("WB_WaterQuality.csv")
+head(water.quality)
 
-trout.avg.names <- paste(trout.avg$Locus.ID)
-trout.avg$names <- trout.avg.names
-trout.avg.means <- aggregate(x = trout.avg, by = list(trout.avg$names), FUN = "mean")
-head(trout.avg.means)
+walleye.local.names <- paste(walleye.local$lattitude, walleye.local$longitude)
+walleye.local$names <- walleye.local.names
+walleye.local.means <- aggregate(x = walleye.local, by = list(walleye.local$names), FUN = "mean")
+head(walleye.local.means)
+
+water.quality.names <- paste(water.quality$lat, water.quality$long)
+water.quality$names <- water.quality.names
+water.quality.means <- aggregate(x = water.quality, by = list(water.quality$names), FUN = "mean")
+head(water.quality.means)
+
+walleye.local.means1 <- walleye.local.means[,-8:-9]
+walleye.local.means2 <- walleye.local.means1[,-3:-5]
+#walleye.local.means3 <- walleye.local.means2[2:29959,]
+head(walleye.local.means2)
+
+water.quality.means1 <- water.quality.means[,-7]
+water.quality.means2 <- water.quality.means1[,-20]
+water.quality.means3 <- water.quality.means2[,-12:-13]
+head(water.quality.means3)
+View(water.quality.means3)
